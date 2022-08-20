b2c927392779663b46d1dd5ab998ae107167f3a8
xiaoxunyao <huchengyea@163.com>
2022-08-20 22:08:28 +0800

    fix: update core

---
+++ core/vite.config.ts
@@ -0,0 +1,40 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import distCss from '@elonehoo/vite-plugin-dist-css'
+import types from '@elonehoo/vite-plugin-type-ts'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    distCss(),
+    types({
+      tsConfigFilePath: './tsconfig.json',
+      cleanVueFileName: true,
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'icon',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
