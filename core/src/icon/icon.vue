fafe647b40af37e0c4a8fb3e0489897df1ffe62f
xiaoxunyao <huchengyea@163.com>
2022-08-21 16:08:11 +0800

    fix: update

--- core/src/icon/icon.vue
+++ core/src/icon/icon.vue
@@ -1,11 +1,83 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { loadIcon, getIcon } from '../util/iconify'
+import type {FullIconCustomisations} from '@iconify/utils/lib/customisations'
+import { iconToSVG,replaceIDs } from '@iconify/utils'
+import { encodeSvgForCss } from '@iconify/utils/lib/svg/encode-svg-for-css'
 
+const props = withDefaults(defineProps<{
+  iconName: string,
+  size?: number | string,
+}>(), {
+  size:1.2,
+})
+
+let collection = ''
+let name = ''
+if (props.iconName.includes(':')) {
+  [collection, name] = props.iconName.split(':')
+}
+
+loadIcon(props.iconName)
+
+const iconData = getIcon(props.iconName)
+if (!iconData) {
+   throw new Error(`Icon "${name}" is missing`);
+}
+
+const options:FullIconCustomisations = {
+  // Display mode
+	inline: false,
+
+	// Dimensions
+	width: '19px',
+	height: '19px',
+
+	// Alignment
+	hAlign: 'center',
+	vAlign: 'middle',
+	slice: false,
+
+	// Transformations
+	hFlip: false,
+	vFlip: false,
+	rotate: 0,
+}
+const renderData = iconToSVG(iconData,options)
+
+const svgAttributes: Record<string, string> = {
+   'xmlns': 'http://www.w3.org/2000/svg',
+   'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+   ...renderData.attributes,
+};
+const svgAttributesStr = Object.keys(svgAttributes)
+   .map(
+       (attr) =>
+           // No need to check attributes for special characters, such as quotes,
+           // they cannot contain anything that needs escaping.
+           `${attr}="${svgAttributes[attr as keyof typeof svgAttributes]}"`
+   )
+   .join(' ');
+
+//Generate SVG
+const svg = `<svg ${svgAttributesStr}>${replaceIDs(renderData.body)}</svg>`;
+const uri = ref(`url("data:image/svg+xml;utf8,${encodeSvgForCss(svg)}")`)
+onMounted(()=>{
+  document.styleSheets[0].insertRule(`.${collection}-${name} {
+    background:${uri.value} no-repeat;
+    -webkit-background:${uri.value} no-repeat;
+    background-size:100% 100%;
+    -webkit-background-size:100% 100%;
+    background-color:transparent;
+    width:${props.size}em;
+    height:${props.size}em;
+  }`)
+})
 </script>
-
 <template>
-  <div />
+  <div :class="`${collection}-${name}`"/>
 </template>
 
 <style scoped>
-.elonehoo{}
+.elone-hoo{}
 </style>
---
+++ example/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { icon } from '@elonehoo/icon'
+</script>
+
+<template>
+  <main class="font-sans px-4 py-10">
+    <icon icon-name="logos:vue"/>
+    <icon icon-name="carbon:3d-cursor-alt"/>
+    <icon icon-name="uil:atom" />
+  </main>
+</template>
+
+<style>
+.font-sans {
+  font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+}
+.px-4 {
+  padding-left: 1rem; /* 16px */
+  padding-right: 1rem; /* 16px */
+}
+.py-10 {
+  padding-top: 2.5rem; /* 40px */
+  padding-bottom: 2.5rem; /* 40px */
+}
+.center {
+  text-align: center;
+}
+</style>
